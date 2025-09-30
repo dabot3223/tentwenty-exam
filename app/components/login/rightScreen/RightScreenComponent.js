dc06da8a8bf618469058a015b2380c39fb7e2dfc
Rameet Khorjuvekar <dabot3223@gmail.com>
2025-09-30 07:40:47 +0530

    corrections mobile login

--- app/components/login/rightScreen/RightScreenComponent.js
+++ app/components/login/rightScreen/RightScreenComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightScreenComponent = () => {
   return (
-    <div style={{ backgroundColor: "#1A56DB" }} className='order-1 md:order-2 max-h-96 md:h-screen' >
+    <div style={{ backgroundColor: "#1A56DB" }} className='order-1 md:order-2 h-96 md:h-screen ' >
       <div className=' text-white h-full flex justify-center flex-col p-16 '>
         <h1 className=' text-4xl font-semibold' >ticktock</h1>
         <p className='text-base font-thin pt-3' >Introducing ticktock, our cutting-edge timesheet web application designed to revolutionize how you manage employee work hours. With ticktock, you can effortlessly track and monitor employee attendance and productivity from anywhere, anytime, using any internet-connected device.</p>
